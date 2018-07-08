53b64e43e8713c1d979279da0131f9ac70e9b69a
oleg <oleg@example.com>
2018-07-09 00:45:18 +0300

    remove model

--- src/main/java/restapp/controller/UserController.java
+++ src/main/java/restapp/controller/UserController.java
@@ -2,11 +2,14 @@ package restapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import restapp.controller.model.User;
+import restapp.dao.User;
 import restapp.repository.UserJpaRepository;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -18,7 +21,14 @@ public class UserController {
     consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
     private User addUser(@RequestBody User user) {
-        repository.save(new restapp.dao.User());
+        repository.save(user);
         return user;
+    }
+
+    @GetMapping(value = "/user",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    private List<User> getUsers() {
+        return repository.findAll();
     }
 }
--- src/main/java/restapp/dao/User.java
+++ src/main/java/restapp/dao/User.java
@@ -16,7 +16,6 @@ public class User {
     private Date birthDate;
     private String email;
 
-
     public User() {
     }
 
@@ -31,39 +30,19 @@ public class User {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
     }
 
     public String getSurname() {
         return Surname;
     }
 
-    public void setSurname(String surname) {
-        Surname = surname;
-    }
-
     public Date getBirthDate() {
         return birthDate;
     }
 
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
